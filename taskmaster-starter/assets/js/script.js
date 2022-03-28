f42185adf5daf6996a94bd1d4c758e1f7d96e6ce
BismuthBetel <97366538+BismuthBetel@users.noreply.github.com>
2022-03-28 15:55:14 -0500

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -41,7 +41,7 @@ var auditTask = function (taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 
-
+  console.log(taskEl);
 
 };
 
@@ -193,7 +193,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -231,15 +231,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
 
@@ -280,14 +286,17 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
 
     ui.draggable.remove();
   },
   over: function (event, ui) {
     console.log("ui");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
     console.log("ui");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -295,6 +304,11 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 
 
